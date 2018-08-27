a69ff2e925374e67a909d6d8eacc97a25d4dd629
Alex Daigre <alexdaigre@gmail.com>
2018-08-27 17:59:21 -0500

    changed to reading data out and storing it in a list. Continued working on implementation

--- DaigreHW1.py
+++ DaigreHW1.py
@@ -1,7 +1,7 @@
 # Alex Daigre
 # Aug 27th, 2018
 # cs4500
-# Version: Python 3.6
+# Version: Python 3.7
 # Description: This program takes an imput file containing unformated times, 
 #   formats them and privides the diffrences between the pairs of times.
 # External Files:
@@ -11,53 +11,68 @@
 #   https://stackoverflow.com/questions/179369/how-do-i-abort-the-execution-of-a-python-script
 #   https://docs.python.org/3/library/re.html#re.compile
 #   https://regexr.com/
+#   https://www.w3schools.com/python/python_lists.asp
+#   https://docs.python.org/3/library/stdtypes.html#str.rstrip
+#   
 
 import sys
 import re
 
-def openInputFile():
+def openInputFileAndGetContents():
     fileName = "HW1input.txt"
-    return open(fileName, 'r')
+    fileContents = []
+    with open(fileName, 'r') as inputFile:
+        for line in inputFile:
+            fileContents.append(line.rstrip())
+    return fileContents
+
+def openOutputFileAndWriteContents(outputData):
+    fileName = "HW1output.txt"
+    with open(fileName, 'w') as outputFile:
+        for line in outputData:
+            outputFile.write(line+"\n")
     
-def verifyInputFile(inputFile):
-    linecount = getFileLineCount(inputFile)
+def verifyFileContents(fileContents, outputData):
+    linecount = len(fileContents)
     if linecount <= 0:
+        outputData.append("Input file was empty.")
         print("Input file was empty.")
         return False
-    #Unsure about spec
-    # elif linecount > 40:
-    #     print("Input has greater than 40 lines.")
-    #     return False
-    # elif (linecount % 2) != 0:
-    #     print("Input does not .")
-    #     return False
-    for line in inputFile:
-        print("Verifining line: ", line)
+    elif linecount > 40:
+        outputData.append("Input has greater than 40 lines, please use between 0 and 40 lines.")
+        print("Input has greater than 40 lines, please use between 0 and 40 lines.")
+        return False
+    for line in fileContents:
         if checklineFormat(line) != True:
-            print("This input was not correct: ", line)
+            outputData.append("This input was not correct: " + line)
             return False
     return True
 
 def getFileLineCount(inputFile):
     totalLines = 0
     for line in inputFile:
-        print("Counting line: ", line)
         totalLines += 1
-    print("Line count: ", totalLines)
+    print("Line count: " + totalLines)
     return totalLines
 
 def checklineFormat(line):
-    lineRegExp = re.compile("/((?!24:)[0-2][0-9]:[0-5][0-9]\n)/g")
+    lineRegExp = re.compile("((?!2[4-9]:)[0-2][0-9]:[0-5][0-9])")
     if lineRegExp.match(line):
+        print("Verified: ", line)
         return True
     else:
-        print("This input was not correct: ", line)
+        print("This input was not correct: " + line)
         return False
 
 
-inputFile = openInputFile()
-print("File open: ", inputFile)
-if verifyInputFile(inputFile) != True:
+fileContents = openInputFileAndGetContents()
+outputData = []
+print("File open: ", fileContents)
+if verifyFileContents(fileContents, outputData) != True:
+    outputData.append("input file was not verified")
     print("input file was not verified")
-    sys.exit(1)
+else:
+    outputData.append("All time pairs processed: program ending.")
+    print("All time pairs processed: program ending.")
+openOutputFileAndWriteContents(outputData)
 
