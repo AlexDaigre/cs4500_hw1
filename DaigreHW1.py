35efeb85698c83ed627a72be18a77ba369a0d21d
RockThunderheart <alexdaigre@gmail.com>
2018-08-27 23:44:44 -0500

    cleaned up code and added additional comments

--- DaigreHW1.py
+++ DaigreHW1.py
@@ -28,6 +28,8 @@ import re
 import datetime
 import itertools
 
+# Opens our input file and stores the contents in a list.
+# Each line is its own enrty in the list.
 def openInputFileAndGetContents():
     fileName = "HW1input.txt"
     fileContents = []
@@ -36,12 +38,16 @@ def openInputFileAndGetContents():
             fileContents.append(line.rstrip())
     return fileContents
 
+# Opens the output file and writes the contents to it.
+# takes our output data and writes each element in the list to its own line.
 def openOutputFileAndWriteContents(outputData):
     fileName = "HW1output.txt"
     with open(fileName, 'w') as outputFile:
         for line in outputData:
             outputFile.write(line+"\n")
-    
+
+# Verifies the file contents for correct number of lines and correct 
+# formating of individual lines.
 def verifyFileContents(fileContents, outputData):
     linecount = len(fileContents)
     if linecount <= 0:
@@ -56,13 +62,8 @@ def verifyFileContents(fileContents, outputData):
                 outputData.append("This input was not correct: " + line)
     return outputData
 
-def getFileLineCount(inputFile):
-    totalLines = 0
-    for line in inputFile:
-        totalLines += 1
-    print("Line count: " + totalLines)
-    return totalLines
-
+# Uses a regular expression to check if our data maches the format in 
+# the specifications.
 def checklineFormat(line):
     lineRegExp = re.compile("((?!2[4-9]:)[0-2][0-9]:[0-5][0-9])")
     if lineRegExp.match(line):
@@ -72,18 +73,22 @@ def checklineFormat(line):
         print("This input was not correct: " + line)
         return False
 
+# Here we generate the, non-error output to write to our file.
+# We loop over the the data we got from our input file, find the
+# time diffrences and format all resulting data, then handle whether our data
+# had an even or odd number of lines  
 def generateOutput(fileContents, outputData):
     for index, times in enumerate(pairwise(fileContents)):
-        #the pairwise itterator returns every element compaired with the next:
-        #eg. s -> (s0, s1) (s1, s2) etc. However we only want everyother pair 
+        # The pairwise itterator returns every element compaired with the next:
+        # eg. s -> (s0, s1) (s1, s2) etc. However we only want everyother pair 
         # like this so we only run the code every other itteration
         if index % 2 == 0:
             timeDiffrence = getTimeDifference(times[0], times[1])
             outputData.append(f"{times[0]}, {times[1]} => {timeDiffrence}")
             print(f"{times[0]}, {times[1]} => {timeDiffrence}")
-    #The itterator raises stop iteration on last element of stop iteration 
-    #as a full pair cannot be generated. As such, we handle that last element here.
-    #If the list was even, we add the appropriate message here
+    # The itterator raises stop iteration on last element of stop iteration 
+    # as a full pair cannot be generated. As such, we handle that last element here.
+    # If the list was even, we add the appropriate message here
     if len(fileContents) % 2 != 0:
         outputData.append(f"There was not a matching time for this time: {fileContents[len(fileContents)-1]}")
         print(f"There was not a matching time for this time: {fileContents[len(fileContents)-1]}")
@@ -92,18 +97,18 @@ def generateOutput(fileContents, outputData):
         print("All time pairs processed: program ending.")
     return outputData
 
-# this is one of the recipies from https://docs.python.org/3/library/itertools.html#itertools-recipes
-# we use it to generate an itterator that returns each element mached with the next 
+# This is one of the recipies from https://docs.python.org/3/library/itertools.html#itertools-recipes
+# We use it to generate an itterator that returns each element mached with the next 
 # element.
 def pairwise(iterable):
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
 
-#We transfor each of our times to a dateTime object, then perform the subtraction,
-#and then we transform the resulting timeDelta back into a formated string.
-#When generating the output, as we have no dates, we always assume the shortest 
-#time between the two dates.
+# We transfor each of our times to a dateTime object, then perform the subtraction,
+# and then we transform the resulting timeDelta back into a formated string.
+# When generating the output, as we have no dates, we always assume the shortest 
+# time between the two dates.
 def getTimeDifference(time1, time2):
     pythonTime1 = transformStringTimeToPythonTime(time1)
     pythonTime2 = transformStringTimeToPythonTime(time2)
@@ -112,15 +117,19 @@ def getTimeDifference(time1, time2):
         timeDiffrence = datetime.timedelta(days=0,seconds=timeDiffrence.seconds, microseconds=timeDiffrence.microseconds)
     return transformPythonTimeDiffrenceToStringTime(timeDiffrence)
 
+# Transforms our string time to the python datetime object
 def transformStringTimeToPythonTime(time):
     return datetime.datetime.strptime(time,"%H:%M")
 
-#Take the TimeDiffrence object, cast to a string, then trim the seconds off.
+# Take the TimeDiffrence object, cast to a string, then trim the seconds off.
 def transformPythonTimeDiffrenceToStringTime(timeDiffrence):
     timeDiffrenceString = str(timeDiffrence)
     return timeDiffrenceString[:timeDiffrenceString.rindex(':')]
 
 
+# The main section of our program
+# we read the contents, verify them, generate the data we want to output,
+# and finaly write it to file.
 outputData = []
 fileContents = openInputFileAndGetContents()
 print("File open: ", fileContents)
